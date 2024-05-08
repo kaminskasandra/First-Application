90bbb59f2e1cc82ba34ad11e22a3c1b05d863820
kaminskasandra <san.porombka@gmail.com>
2024-05-08 16:21:32 +0200

    task 30.3

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -33,6 +33,7 @@ public class EmailScheduler {
                         SUBJECT,
                         "Currently in database you got: " + size + " " + version
                 )
+
         );
     }
 }
